2869bc1cd11adf2ecec39c912c2d7b4c0e99065e
TheThonk <35922143+TheThonk@users.noreply.github.com>
2018-02-12 16:29:52 +0300

    Delete UsersList.js

--- src/components/UsersList.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import App from '../App.js';
-
-export default function UsersList({ users }) {
-  const userElems = users.map(user =>
-    <li key = {user.id}>
-  )
-}
